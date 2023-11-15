48c54d406d3476aebecf752c4e35a6c50193b4b0
Bradley Gallagher <bradley.gallagher@and.digital>
2023-11-15 15:59:26 +0000

    Home page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Navbar from "./components/Navbar/Navbar"
+import HeroImageBottom from "./components/Hero-Image-Bottom/Hero-Image-Bottom";
+import HeroImageTop from "./components/Hero-Image-Top/Hero-Image-Top";
+import HeroText from "./components/Hero-Text/Hero-Text";
 
 export default function Home() {
   return (
     <div>
-    <h1> Test file (Index is page.tsx) </h1>
+      <h1 text-3xl font-bold underline > Iriwn Mitchell</h1>
+      <HeroImageTop />
+      <HeroText />
+      <HeroImageBottom />
     </div>
-  )
+  );
 }
