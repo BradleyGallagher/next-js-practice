81337502397fd0c73100158b5bea39ab96e05203
Bradley Gallagher <bradley.gallagher@and.digital>
2023-11-16 10:58:31 +0000

    Lists page configured

---
+++ app/list/page.tsx
@@ -0,0 +1,12 @@
+import Tile from "../components/Tile/Tile";
+
+export default function List() {
+  return (
+    <div>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+    </div>
+  );
+}
